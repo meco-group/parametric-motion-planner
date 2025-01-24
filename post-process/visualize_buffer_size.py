382a64490f2f2be4a4c6f1768fdf5df9f9c662ec
Wilm Decré <wilm.decre@kuleuven.be>
2025-01-24 11:40:04 +0100

    minor

--- post-process/visualize_buffer_size.py
+++ post-process/visualize_buffer_size.py
@@ -6,35 +6,30 @@ def visualize_buffer_size_over_time(data):
     Tf = data["travelled_trajectory"]["Tf"]
     dt = data["travelled_trajectory"]["dt"]
     sample_durations = data["ms_to_retrieve_sample"]
-    print(sample_durations)
-
-    # make a histogram of the sample durations (bin width 1)
-    plt.figure()
-    plt.hist(sample_durations, bins=range(0, int(max(sample_durations))+1), edgecolor='black')
 
     sample_durations = [0.001*s for s in sample_durations]
     nb_samples = len(sample_durations)
 
     # construct a time-grid vector going from 0 to Tf with steps dt/refinment
     refinment = 10
-    time_grid = np.arange(-0.5, Tf + 2, dt/refinment)
+    time_grid = np.arange(-0.5, Tf + 1, dt/refinment)
 
     # construct a buffer size vector
     buffer_size = np.zeros(len(time_grid))
 
     # fill the buffer size vector
+    number_of_samples_provided_to_mover = 0
     for i in range(len(time_grid)):
         t = time_grid[i]
         # number_of_samples_read_by_mover = max(0, int(min(t/data["travelled_trajectory"]["dt"],
         #                                                  data["travelled_trajectory"]["nb_samples"])))
         number_of_samples_read_by_mover = max(0, int(t/data["travelled_trajectory"]["dt"]))
 
-        number_of_samples_provided_to_mover = 0
         while (number_of_samples_provided_to_mover < len(data["duration_of_request_since_first_sample_in_ms"]) and
                t + data["mover_started_moving"]/1000 > data["duration_of_request_since_first_sample_in_ms"][number_of_samples_provided_to_mover]/1000):
             number_of_samples_provided_to_mover += 1
 
-        buffer_size[i] = number_of_samples_provided_to_mover - number_of_samples_read_by_mover
+        buffer_size[i] = max(0, number_of_samples_provided_to_mover - number_of_samples_read_by_mover)
         
     
     # plot the buffer size over time
@@ -42,6 +37,8 @@ def visualize_buffer_size_over_time(data):
     plt.plot(time_grid, buffer_size)
     plt.xlabel("Time [s]")
     plt.ylabel("Number of samples in buffer")
+    plt.ylim([-1, max(buffer_size) + 1])
+    plt.gca().grid(axis='y')
     plt.show()
 
 file = "build/output/dynamic_solution_movable_destination_sampler.json"
